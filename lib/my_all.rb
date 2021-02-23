6e0468f1b6ce5e0d70499038d906a123062d7ca6
Vince W <vincewallgren@me.com>
2021-02-23 05:03:13 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,9 @@
-require 'pry'
 
-def my_all?(collection)
 
+ddef my_all?(collection)
+  i = 0
+  while i < collection.length
+    binding.pry
+    i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
+  end
 end
